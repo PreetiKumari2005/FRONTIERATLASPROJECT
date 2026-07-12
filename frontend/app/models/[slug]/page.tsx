cbc0add8332e3102f2ea5d88590ab948323ff3ef
Ankit <231080385+ankitt-11@users.noreply.github.com>
2026-07-12 19:52:09 +0530

    Fixed Error to frontend doesnt load in locally

--- frontend/app/models/[slug]/page.tsx
+++ frontend/app/models/[slug]/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-export const runtime = "edge";
 import { useCallback, useEffect, useRef, useState } from "react";
 import { useParams } from "next/navigation";
 import Link from "next/link";
